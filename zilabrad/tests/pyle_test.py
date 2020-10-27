4c05c4471cd07f249c883af0e3e4ba328bfb25c9
zeeutao <zeeutao@163.com>
2020-10-27 20:16:53 +0800

    Update pyle_test.py

--- zilabrad/tests/pyle_test.py
+++ zilabrad/tests/pyle_test.py
@@ -7,7 +7,7 @@ import logging
 import time
 import numpy as np
 
-from zilabrad.instrument.qubitServer import loadQubits,RunAllExperiment
+from zilabrad.instrument.qubitServer import loadQubits,Unit2SI,Unit2num
 from zilabrad.instrument.qubitServer import runQubits
 
 from zilabrad.instrument import qubitServer
@@ -15,7 +15,7 @@ from zilabrad.instrument import qubitServer
 from zilabrad.pyle import sweeps
 from zilabrad.pyle.sweeps import gridSweep,checkAbort
 from zilabrad.pyle.util import sweeptools
-
+from zilabrad.pyle.workflow import switchSession
 # for physical units, GHz, ns, ...
 from labrad.units import Unit,Value
 
@@ -24,6 +24,8 @@ _unitSpace = ('V','mV','us','ns','s','GHz','MHz','kHz','Hz','dBm','rad','None')
 V, mV, us, ns,s, GHz, MHz,kHz,Hz, dBm, rad,_l = [Unit(s) for s in _unitSpace]
    
 ar = sweeptools.r
+
+_noisy_printData = True
 
 def runDummy(qubits,exp_devices):
     data = np.array([])
@@ -36,10 +38,58 @@ def runDummy(qubits,exp_devices):
             data = np.vstack([data,data_new])
     return data
 
+def RunAllExperiment(function,iterable,dataset,
+                     collect: bool = True,
+                     raw: bool = False,
+                     pipesize: int = 10):
+    """ Define an abstract loop to iterate a funtion for iterable
+
+    Example:
+    prepare parameters: scanning axes, dependences and other parameters
+    start experiment with special sequences according to the parameters
+    
+    Args:
+        function: give special sequences, parameters
+        iterable: iterated over to produce values that are fed to function as parameters.
+        dataset: zilabrad.pyle.datasaver.Dataset, object for data saving
+        collect: if True, collect the result into an array and return it; else, return an empty list
+        raw: discard swept_paras if raw == True
+    """
+    def run(paras):
+        # pass in all_paras to the function
+        all_paras = [Unit2SI(a) for a in paras[0]]
+        swept_paras = [Unit2num(a) for a in paras[1]]
+        result = function(None,all_paras) # 'None' is just the old devices arg which is not used now 
+        if raw:
+            result_raws = np.asarray(result)
+            return result_raws.T
+        else:
+            result = np.hstack([swept_paras,result])
+            return result
+
+    def wrapped():
+        for paras in iterable:
+            result = run(paras)
+            if _noisy_printData == True:
+                print(
+                    str(np.round(result,3))
+                    )
+            yield result
+    
+
+    results = dataset.capture(wrapped())
+        
+    result_list = []
+    for result in results:
+        result_list.append(result)
+    
+    if collect:
+        return result_list
+        
 def RunAllDummy(*args):
     exp_devices = [None]*4
-    qubitServer._noisy_printData = False
-    data = qubitServer.RunAllExperiment(exp_devices,*args)
+    _noisy_printData = False
+    data = RunAllExperiment(*args)
     return data
 
 def stop_device():
@@ -92,7 +142,7 @@ def testing_runQ(sample,measure=0,stats=1024,freq=6.0*GHz,delay=0*ns,power=-30.0
         return result 
 
     axes_scans = checkAbort(gridSweep(axes), prefix=[1],func=stop_device)
-    result_list = RunAllDummy(runSweeper,axes_scans)
+    result_list = RunAllDummy(runSweeper,axes_scans,dataset)
     if back:
         return result_list
 
