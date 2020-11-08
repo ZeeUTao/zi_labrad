862c55f4f582ae64f253d0bb1d812cec7b2b4a5f
Ziyu Tao <zeeutao@163.com>
2020-11-08 18:09:17 +0800

    Update __init__.py

--- zilabrad/__init__.py
+++ zilabrad/__init__.py
@@ -1,55 +1,49 @@
-from __future__ import division, print_function, absolute_import
-import os
-import sys
-import warnings
-
-
-# -----------------------------------------------------------------------------
-# Check if we're in IPython.
-
-
 
 
 """Set up the main namespace."""
 
 __all__ = [
-	'mp',
-	'zurichHelper',
-	'switchSession',
-	'waveforms'
+    'ar',
+    'multiplex',
+    'dataProcess',
+    'connect_ZI',
+    # for developer
+    'qubitContext',
+    'qubitServer',
+	'waveforms',
+    'zurichHelper',
 ]
-
-
-# from zilabrad import multiplex as mp
-from zilabrad.instrument import zurichHelper
-from zilabrad.pyle.workflow import switchSession
-from zilabrad.instrument import waveforms
-
-from zilabrad.pyle.registry import RegistryWrapper
-
-
-"""
-Example:
-	ar[1.0:2.0:0.01,GHz]
-"""
 
 
 # -----------------------------------------------------------------------------
 # Load modules
 #
+from zilabrad.instrument import waveforms
 
-# core
+# example: ar[0:2:0.1,GHz]
+from zilabrad.pyle.util import sweeptools
+ar = sweeptools.RangeCreator()
+
+from zilabrad.instrument import zurichHelper
+from zilabrad.instrument import qubitServer
+from zilabrad.instrument.QubitContext import qubitContext
+from zilabrad.instrument.QubitContext import update_session
+from zilabrad.plots import dataProcess
+from zilabrad import multiplex
 
 
-
-
-
-
-
-
-
+def connect_ZI():
+    global session
+    user = input("Enter user (default: hwh)") or "hwh"
+    session = update_session(user=user)
+    do_bringup = input("Skip Bringup? (enter 0 for skip, default 1)") or 1
+    if do_bringup != '0':
+        qctx = qubitContext()
+        qctx.refresh()
+    return
 
 
 # -----------------------------------------------------------------------------
 # Clean name space
 #
+
