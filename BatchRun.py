c7525261bfdeaec96dc1e0b2295fed6c0eb07167
Ziyu Tao <zeeutao@163.com>
2020-11-12 18:06:02 +0800

    Update BatchRun.py

--- BatchRun.py
+++ BatchRun.py
@@ -6,47 +6,23 @@ It will bringup the devices (object), and store some daily commands
 """
 
 
-
 from importlib import reload
-import os
 import numpy as np
-from zilabrad.pyle.registry import RegistryWrapper
-from zilabrad.instrument import zurichHelper as zH
 
-from zilabrad.pyle.util import sweeptools as st
-
-from zilabrad.instrument import waveforms
-import zilabrad.instrument.qubitServer as qubitServer
-from zilabrad.instrument.QubitContext import qubitContext
-from zilabrad.instrument.QubitContext import update_session
-from zilabrad import multiplex as mp
-from zilabrad.plots import dataProcess as dp
-
-
-import labrad
+from zilabrad import *
 from labrad.units import Unit,Value
 
-ar = st.RangeCreator()
 _unitSpace = ('V','mV','us','ns','s','GHz','MHz','kHz','Hz','dBm','rad','None')
 V, mV, us, ns,s, GHz, MHz,kHz,Hz, dBm, rad,_l  = [Unit(s) for s in _unitSpace]
+# some useful units
+
+mp = multiplex
+dp = dataProcess
+# add some abbreviated to use
 
 dh = dp.datahelp()
 
-def bringup_device():
-    qctx = qubitContext()
-    qctx.refresh()
-    return       
-
-
 
 if __name__ == '__main__':
-    user = input("Enter user (default: hwh)") or "hwh"
-    ss = update_session(user=user)
-    
-    dh = dp.datahelp(path = None,session=ss._dir)
-    do_bringup = input("Skip Bringup? (enter 0 for skip, default 1)") or 1
-    if do_bringup != '0':
-        bringup_device()
-    from zilabrad.instrument import zurichHelper
-    reload(zurichHelper)
+    sample = connect_ZI(reset=False)
 
