a62d37055616102369b566401d437fab7e693375
zeeutao <zeeutao@163.com>
2021-05-02 03:11:46 +0800

    Update __init__.py

--- zilabrad/__init__.py
+++ zilabrad/__init__.py
@@ -4,6 +4,7 @@ Modular quantum control for zurich instrument
 
 
 # set up the main namespace.
+import labrad
 from zilabrad.instrument.QubitContext import qubitContext
 from zilabrad.instrument import zurichHelper
 from zilabrad.instrument import waveforms
@@ -12,8 +13,8 @@ from zilabrad.pyle.util import sweeptools
 from zilabrad.pyle.workflow import switchSession
 
 
-# from zilabrad import multiplex
-# from zilabrad.plots import dataProcess
+from zilabrad import multiplex
+from zilabrad.plots import dataProcess
 
 """
 Author: Ziyu Tao, WenHui Huang
@@ -27,7 +28,7 @@ __all__ = [
     'ar',
     'multiplex',
     'dataProcess',
-    'connect_ZI',
+    'login',
     # for developer
     'qubitContext',
     'qubitServer',
@@ -35,27 +36,16 @@ __all__ = [
     'zurichHelper',
 ]
 
-import labrad
 cxn = labrad.connect()
 
-def login(user='hwh',bringup=False):
-    ## make sure login user name
+
+def login(user='hwh', bringup=False):
+    # make sure login user name
     user_input = input(f"Enter user (default: {user})") or user
-    ## load sample register from local
+    # load sample register from local
     sample = switchSession(cxn, user=user, session=None)
-    ## create device management instance and give sample
-    qc = qubitContext(cxn=cxn,user_sample=sample)
+    # create device management instance and give sample
+    qc = qubitContext(cxn=cxn, user_sample=sample)
     if bringup:
         qc.refresh()
     return sample
-
-
-
-# def connect_ZI(user='hwh'):
-#     user_input = input(f"Enter user (default: {user})") or user
-#     sample = update_session(user=user_input)
-#     do_bringup = input("Skip Bringup? (enter 0 for skip, default 1)") or 1
-#     if do_bringup != '0':
-#         qctx = qubitContext(user_sample=sample)
-#         qctx.refresh()
-#     return sample
